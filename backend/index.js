5852038cbab1f58b30af88a641d2a039dbb46c55
reallyelvik <elviksharma111@gmail.com>
2025-04-11 12:13:14 +0545

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+//middleware for parsing request body
+app.use(express.json());
+
+//middleware for handling cora policy
+app.use(
+     cors({
+          origin: "http://localhost:5173",
+          methods: ["GET", "POST", "PUT", "DELETE"],
+          allowedHeaders: ["Content-Type"],
+     })
+);
+
+app.get("/", (req, res) => {
+     console.log(req);
+     return res.status(234).send("Welcome");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+     .connect(mongoDBURL)
+     .then(() => {
+          console.log("App connected to database");
+          app.listen(PORT, () => {
+               console.log(`App is listening on port: ${PORT}`);
+          });
+     })
+     .catch((error) => {
+          console.log(error);
+     });
